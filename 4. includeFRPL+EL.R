7cedbc3b901ab3774228c785af5b693ff8f0029d
Evan Rushton <evan.rushton@gmail.com>
2018-10-07 22:40:15 -0700

    discovering colnames for FRPL

--- 4. includeFRPL+EL.R
+++ 4. includeFRPL+EL.R
@@ -14,25 +14,75 @@ source("./functions.R")
 # English Learner (EL) data
 # EL Data (1980-2018: https://www.cde.ca.gov/ds/sd/sd/fileselsch.asp File structure: https://www.cde.ca.gov/ds/sd/sd/fselsch.asp
 # TOTAL variable starting in 95 and changing in 98
+
+# Create a list of files to combine 
+el0117.list <- fileList("./Data/CA", "^elsc.*txt$") # 2001, 2007-2017 have headers
+el0206.nh.list <- fileList("./Data/CA", "^elsn.*txt$") # 2002-2006 have no header
+el8000.list <- fileList("./Data/CA", "^els.*csv$") # 1980 has extra column
+
+# Make consistent EL colnames (21 variables)
+options(scipen = 16)  # Make full CDS_CODE visible
+colnames <- c("CDS_CODE", "COUNTY", "DISTRICT", "SCHOOL", "LC", "LANGUAGE", "KDGN", "GR_1", "GR_2", "GR_3", "GR_4", "GR_5", "GR_6", "GR_7", "GR_8", "GR_9", "GR_10", "GR_11", "GR_12", "UNGR", "TOTAL")
+
+# Convert file list into a data.table with an ID row (Using starting year as the year indicator)
+DT.list <- lapply(el8000.list, read.csv, header=TRUE, check.names=FALSE)
+setattr(DT.list, 'names', c("1980", "1981", "1982","1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000"))
+DT.list$`1980`$'CDS_CODE,C,14' <- NULL # Remove duplicate CDS col from 1980
+DT.list <- lapply(DT.list, setNames, colnames)
+DT00 <- rbindlist(DT.list, use.names=TRUE, fill=TRUE, idcol='YEAR') # creates data.table from list of data.frames
+
+DT.list <- lapply(el0117.list, read.table, fill=TRUE, na.strings=c("", "NA"), sep ="\t", quote = "", header=TRUE, check.names=FALSE)
+setattr(DT.list, 'names', c("2001", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"))
+DT.list <- lapply(DT.list, setNames, colnames)
+DT17 <- rbindlist(DT.list, use.names=TRUE, fill=TRUE, idcol='YEAR') # creates data.table from list of data.frames
+
+DT.list <- lapply(el0206.nh.list, read.table, fill=TRUE, na.strings=c("", "NA"), sep ="\t", quote = "", header=FALSE, check.names=FALSE)
+setattr(DT.list, 'names', c("2002", "2003", "2004", "2005", "2006"))
+DT.list <- lapply(DT.list, setNames, colnames)
+DT06 <- rbindlist(DT.list, use.names=TRUE, fill=TRUE, idcol='YEAR') # creates data.table from list of data.frames
+
+DTel <- rbind(DT00, DT17, DT06) # combine 3 data tables into one EL table for starting years 1980 - 2017
+
 # Free/Reduced Price Lunch (FRPL) data
 # FRPL Data (2004-2018): https://www.cde.ca.gov/ds/sd/sd/filessp.asp File structure changes every year until 2014: https://www.cde.ca.gov/ds/sd/sd/fsspfrpm.asp
 # FRPL Data (1988-2003): https://www.cde.ca.gov/ds/sh/cw/filesafdc.asp File Structure: https://www.cde.ca.gov/ds/sh/cw/fsafdc.asp
 
 # Create a list of files to combine 
-el0117.list <- fileList("./Data/CA", "^els.*txt$") # (years with diff colnames)
-el8000.list <- fileList("./Data/CA", "^els.*csv$") # (csv needs different params)
-colnames <- c("CDS_CODE", "COUNTY", "DISTRICT", "SCHOOL", "LC", "LANGUAGE", "KDGN", "GR_1", "GR_2", "GR_3", "GR_4", "GR_5", "GR_6", "GR_7", "GR_8", "GR_9", "GR_10", "GR_11", "GR_12", "UNGR", "TOTAL")
+fr8803.list <- fileList("./Data/CA", "^af.*csv$") # 1988-2003 (1988-1997 13 variables, 1998 14 variables, 1999-2003 15 variables)
+fr0417.list <- fileList("./Data/CA", "^fr.*csv$") # 2004-2017 (every starting year until 2014 it changed... 2004 20 vars? (14), 2005 13 vars, 2006 14, 2007 14, 2008 14, 2009 14 , 2010 14 , 2011 15, 2012 22, 2013 28, 2014-2017 28 (headers in 2017 first col same as 2013 colnames?))
 
-DT.list <- lapply(el8000.list, read.csv, header=TRUE, check.names=FALSE)
-setattr(DT.list, 'names', c("1980", "1981", "1982","1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000"))
-DT.list$`1980`$CDS_CODE <- NULL # Remove duplicate CDS col from 1980
+# Make consistent FRPL colnames (15 variables)
+options(scipen = 16)  # Make full CDS_CODE visible
+colnames <- c("YEAR", "C", "D", "S", "DISTRICT", "SCHOOL", "GRD_SPAN", "PUBLIC_ENR", "PRIVATE_EN", "CALWORKS", "FREE_MEALS", "RED_MEALS", "TOTAL_MEAL", "CALW_PCT", "MEAL_PCT")
+
+# Convert file list into a data.table with an ID row (year variable is already a column)
+DT.list <- lapply(fr8803.list, read.csv, header=TRUE, check.names=FALSE)
 DT.list <- lapply(DT.list, setNames, colnames)
-DT00 <- rbindlist(DT.list, use.names=TRUE, fill=TRUE, idcol='YEAR') # creates data.table from list of data.frames
+# Add year column and district name to 1998 (what is rank?)
+DT03 <- rbindlist(DT.list, use.names=TRUE, fill=TRUE, idcol='YEAR') # creates data.table from list of data.frames
 
-# Convert file list into a data.table with an ID row (Using starting year as the year indicator)
-DT17 <- listToDataTable(el0117.list, c("2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"), id="YEAR")
-DT00 <- csvListToDataTable(el8000.list, c("1980", "1981", "1982","1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000"), id="YEAR")
+DT.list <- lapply(fr0417.list, read.csv, header=TRUE, check.names=FALSE)
+setattr(DT.list, 'names', c("2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"))
+DT.list <- lapply(DT.list, setNames, colnames)
+DT17 <- rbindlist(DT.list, use.names=TRUE, fill=TRUE, idcol='YEAR') # creates data.table from list of data.frames
+
+# ================== Part 2: Check NA values =============================
+# Missing rows are likely due to school closures
+
+sapply(DT00, function(y) sum(length(which(is.na(y))))) # 0
+sapply(DT06, function(y) sum(length(which(is.na(y))))) # 79 rows, 24 CDS
+sapply(DT17, function(y) sum(length(which(is.na(y))))) # 131 rows, 30 CDS
+sapply(DTel, function(y) sum(length(which(is.na(y))))) # 210 rows, 54 CDS
+length(unique(DTel$CDS_CODE[which(is.na(DTel$LC))]))
 
 
 
  
+
+# ================== Write transformed data to csv =============================
+# Write data tables to csv for others to use and avoid above work
+# Recall the year ranges are kept seperate?
+
+write.csv2(DT92, "./Transformed_Data/CA/el.csv", na = "NA")
+write.csv2(DT97, "./Transformed_Data/CA/frpl.csv", na = "NA")
+
