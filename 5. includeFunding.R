38502f2bf4325a9fba27cfe633fcbc1e598025ef
Evan Rushton <evan.rushton@gmail.com>
2018-11-07 22:39:06 -0800

    Future file

---
+++ 5. includeFunding.R
@@ -0,0 +1,8 @@
+# Include funding formula
+# Should have data here: https://www.cde.ca.gov/fg/fr/eb/
+# Individual school files are here: http://lcapwatch.org/
+# Yearly SARC files are like 2016: https://www.cde.ca.gov/ta/ac/sa/accessdata1617.asp
+# Could create theoretical values from: https://www.cde.ca.gov/fg/aa/pa/ratesandinfo.asp, but they change each year!
+# Check theoretical against averages: https://www.cde.ca.gov/fg/fr/sa/cefavgsalaries.asp
+
+# Still Collecting data!
